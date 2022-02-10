8d58b32c6c317555ba6976364c217ec30d6ff782
dagoull <dcastell@ull.es>
2022-02-10 17:48:37 +0000

    Init

---
+++ CMakeProject_1.cpp
@@ -0,0 +1,14 @@
+﻿// CMakeProject_1.cpp: define el punto de entrada de la aplicación.
+//
+
+#include "CMakeProject_1.h"
+#include "cstring"
+
+using namespace std;
+
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ CMakeProject_1.h
@@ -0,0 +1,8 @@
+﻿// CMakeProject_1.h: archivo de inclusión para archivos de inclusión estándar del sistema,
+// o archivos de inclusión específicos de un proyecto.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Haga referencia aquí a los encabezados adicionales que el programa requiere.
